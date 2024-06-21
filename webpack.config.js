8fdb0f197312969edc0d7e00a47f4d39d57bab17
thepralad <prgurung04@gmail.com>
2024-06-21 23:58:34 +0530

    feat: Configure webpack for bundling and HTML generation

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+        template: './src/index.html'
+    })
+  ]
+};
