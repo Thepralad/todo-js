bed90186e18db28a53297607790b731c32b3126d
thepralad <prgurung04@gmail.com>
2024-06-22 23:23:52 +0530

    chore: Update taskStorage.js to add and remove tasks from local storage

--- src/scripts/taskStorage.js
+++ src/scripts/taskStorage.js
@@ -4,6 +4,11 @@ const Tasks = (function(){
     return{
         addTaskToStorage: function(value){
             tasks.push(value);
+            window.localStorage.setItem(`${value.title}`, JSON.stringify(value));
+    
+        },
+        removeTaskFromStorage: function(value){
+            window.localStorage.removeItem(`${value.title}`);
         },
         showTasks: function(){
             for (const task of tasks) {
