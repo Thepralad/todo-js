da5b1c15c46d5789611272fdbb23764d26a9abda
thepralad <prgurung04@gmail.com>
2024-06-22 23:41:44 +0530

    feat: Add domManupulator function to manipulate DOM elements

---
+++ src/scripts/dom.js
@@ -0,0 +1,16 @@
+export default function domManupulator(value){
+    const title = document.createElement('h3');
+    const dueDate = document.createElement('p');
+    const priority = document.createElement('p');
+    const task = document.createElement('div');
+
+    title.textContent = value.title;
+    dueDate.textContent = value.dueDate;
+    priority.textContent = value.priority;
+
+    task.appendChild(title);
+    task.appendChild(dueDate);
+    task.appendChild(priority);
+    
+    document.body.appendChild(task);
+}
