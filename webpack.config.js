442a2fa5febc2a0975e0a1f977620c22aeffbed7
thepralad <prgurung04@gmail.com>
2024-06-23 10:21:16 +0530

    refactor: Update webpack.config.js entry point to './src/scripts/index.js'

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/scripts/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devtool: 'inline-source-map',
   plugins: [
     new htmlWebpackPlugin({
         template: './src/index.html'
