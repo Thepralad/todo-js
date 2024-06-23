8e46e37da6abc4c41095947b1f051e2ee1aaf7f1
thepralad <prgurung04@gmail.com>
2024-06-23 10:20:37 +0530

    feat: Refactor tasks.js to remove unused showTask function

--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -1,14 +1,12 @@
 import { Tasks } from "./taskStorage";
 
+
 function addTask(value){
     Tasks.addTaskToStorage(value);
-}
-function showTask(){
-    Tasks.showTasks()
 }
 function removeTask(value){
     Tasks.removeTaskFromStorage(value);
 }
 export{
-    addTask, showTask, removeTask
+    addTask,removeTask,
 }
